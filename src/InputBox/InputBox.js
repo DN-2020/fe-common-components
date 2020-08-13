1882e2bd71bb20efe15121adfc73c6e349fb3464
moonchanghee <56143212+moonchanghee@users.noreply.github.com>
2020-08-13 13:58:50 +0900

    Input update

--- src/InputBox/InputBox.js
+++ src/InputBox/InputBox.js
@@ -1,18 +1,29 @@
-import React from 'react';
+import React , {useState}from 'react';
 import 'antd/dist/antd.css';
 import { Input } from 'antd';
+import { action } from '@storybook/addon-actions';
 
-
-const onChange = e => {
-  console.log(e);
-};
 
 const InputBox = () => {
+
   
+  const [info , setinfo] = useState("")
+  const onChange = e => {
+    setinfo(e.currentTarget.value)
+  };
+  
+  const submit = () => {
+    console.log(info)
+  };
+  
+
  return(
   <>
-  <Input.Group style ={{width : "18%" , marginTop : "10%" , marginLeft : "10%"}}>
-    <Input.Search enterButton style={{verticalAlign:'middle'}}  placeholder="name" allowClear onChange={onChange} size = "middle" width = "50%" />
+  <Input.Group style ={{width : "18%"}}>
+    <Input.Search enterButton style={{verticalAlign:'middle'}} 
+     placeholder="name" allowClear onChange={onChange} onSearch = {action('submit')}
+     size = "middle" width = "50%" 
+     value = {info}/>
     </Input.Group>
       </>
 )}
